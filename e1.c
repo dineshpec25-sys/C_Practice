86a3662f8392fcf7b8d880c162c5a2a7301ba708
dineshpec25-sys <dineshp.ec25@bitsathy.ac.in>
2026-04-23 11:50:42 +0530

    Print sum of 1-5

--- e1.c
+++ e1.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    int x;
-    x = 5;
+    int x , y;
+    x = 1;
+    y = 0;
 
-    loop: if(x > 0)
+    loop: if(x < 6)
     {
-        printf("%d\n", x--);
+        y = y + x;
+        x++;
         goto loop;
     }
+    printf("%d\n", y);
 }
