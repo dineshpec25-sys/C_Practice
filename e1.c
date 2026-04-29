b2f70ecbe03bc13fc14a92cb78f25e87df917a83
dineshpec25-sys <dineshp.ec25@bitsathy.ac.in>
2026-04-29 12:55:45 +0530

    Print sum of digits is 7

--- e1.c
+++ e1.c
@@ -6,10 +6,10 @@ int main()
 
     loop: if(x < 100)
     {
-        if(((x%2) != 0) && ((x/10) != 0)){
+        if(((x%2) == 0) && ((x/10) != 0)){
             int lastdigit = x % 10;
             int firstdigit = x / 10;
-            if((lastdigit + firstdigit) == 7)
+            if((lastdigit + firstdigit) == 6)
             {
                 printf("%d\n", x);
             }
