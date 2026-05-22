4fbc4a110108917c28f9e4a629ad825e55f94d8f
dineshpec25-sys <dineshp.ec25@bitsathy.ac.in>
2026-05-22 13:36:25 +0530

    Biginteger Sub

--- src/strings/string_sub.c
+++ src/strings/string_sub.c
@@ -1,31 +1,111 @@
-#include<stdio.h>
-// #include<string.h>
-// #include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-// char* sub(char* n1, char* n2, int s1, int s2);
-// void dis(char* res);
-
-// int main()
-// {
-//     char n1_arr[51];
-//     char n2_arr[51];
-
-//     printf("Enter the value of first string : ");
-//     scanf("%s", n1_arr);
-
-//     printf("Enter the value of first string : ");
-//     scanf("%s", n1_arr);
-
-//     int s1 = strlen(n1_arr);
-//     int s2 = strlen(n2_arr);
-
-//     char* n1 = n1_arr;
-//     char* n2 = n2_arr;
-
-    
-// }
-
-int main()
+char *bigint_sub(char *input_arr1, char *input_arr2,
+                 int size_arr1, int size_arr2)
 {
-    printf("Hello world");
+    int master_count =
+        (size_arr1 > size_arr2) ? size_arr1 : size_arr2;
+
+    char local_array1[master_count + 1];
+    char local_array2[master_count + 1];
+
+    /* left zero padding */
+    for(int i = 0; i < master_count; i++)
+    {
+        if(i < master_count - size_arr1)
+        {
+            local_array1[i] = '0';
+        }
+        else
+        {
+            local_array1[i] =
+                input_arr1[i - (master_count - size_arr1)];
+        }
+    }
+    local_array1[master_count] = '\0';
+
+    for(int i = 0; i < master_count; i++)
+    {
+        if(i < master_count - size_arr2)
+        {
+            local_array2[i] = '0';
+        }
+        else
+        {
+            local_array2[i] =
+                input_arr2[i - (master_count - size_arr2)];
+        }
+    }
+    local_array2[master_count] = '\0';
+
+    /* compare and swap if needed */
+    int check_interchange = 0;
+
+    for(int i = 0; i < master_count; i++)
+    {
+        if(local_array1[i] < local_array2[i])
+        {
+            for(int j = 0; j < master_count; j++)
+            {
+                char temp = local_array1[j];
+                local_array1[j] = local_array2[j];
+                local_array2[j] = temp;
+            }
+
+            check_interchange = 1;
+            break;
+        }
+
+        else if(local_array1[i] > local_array2[i])
+        {
+            break;
+        }
+    }
+
+    /* subtraction */
+    char *result = malloc(master_count + 1);
+
+    int count = master_count;
+    result[count--] = '\0';
+
+    int digit_index = master_count - 1;
+    int borrow = 0;
+
+    for(int i = 0; i < master_count; i++)
+    {
+        int left_digit =
+            (local_array1[digit_index] - '0') - borrow;
+
+        int right_digit =
+            local_array2[digit_index] - '0';
+
+        borrow = 0;
+
+        if(left_digit < right_digit)
+        {
+            left_digit += 10;
+            borrow = 1;
+        }
+
+        result[count--] =
+            (left_digit - right_digit) + '0';
+
+        digit_index--;
+    }
+
+    /* add minus sign */
+    if(check_interchange == 1)
+    {
+        result = realloc(result, master_count + 2);
+
+        for(int i = master_count; i >= 0; i--)
+        {
+            result[i + 1] = result[i];
+        }
+
+        result[0] = '-';
+    }
+
+    return result;
 }
