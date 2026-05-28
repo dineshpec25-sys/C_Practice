681f55b73d4e90081b93baf949466cda521ae3f7
dineshpec25-sys <dineshp.ec25@bitsathy.ac.in>
2026-05-29 00:56:14 +0530

    pointer in structure

---
+++ src/structure/pointer.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+struct student
+{
+    int roll;
+    float marks;
+};
+
+void dis(struct student *ptr)
+{
+    printf("The student roll number : %d\n", ptr->roll);
+    printf("The student's marks : %.2f\n", ptr->marks);
+
+    return;
+}
+
+int main()
+{
+    struct student s;
+
+    struct student *p;
+
+    p=&s;
+
+    printf("Enter the roll number : ");
+    scanf("%d", &p->roll);
+    printf("Enter the marks : ");
+    scanf("%f", &p->marks);
+
+    dis(p);
+
+    return 0;
+}
