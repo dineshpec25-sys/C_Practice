800deabb759720cde26f285241f744f293cd8826
dineshpec25-sys <dineshp.ec25@bitsathy.ac.in>
2026-06-04 15:34:02 +0530

    Counting

--- src/bit_manipulation/rotation_counting/count.c
+++ src/bit_manipulation/rotation_counting/count.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <stdint.h>
+#define bit 8
 
 int set_count(uint8_t reg);
-// int zero_count(uint8_t reg);
+int zero_count(uint8_t reg);
 
 int main()
 {
-    uint8_t reg = 0b11110000;
+    uint8_t reg = 0b11111000;
     
     int sc = set_count(reg);
-    // int zc = zero_count(reg);
+    int zc = zero_count(reg);
 
-    printf("The total number of set in the register is %d", sc);
-    // printf("The total number of zero in the register is %d", zc);
+    printf("The total number of set in the register is %d\n", sc);
+    printf("The total number of zero in the register is %d\n", zc);
 
     return 0;
 }
@@ -27,4 +28,11 @@ int set_count(uint8_t reg)
     }
 
     return count;
+}
+
+int zero_count(uint8_t reg)
+{
+    uint8_t result = bit - set_count(reg);
+
+    return result;
 }
