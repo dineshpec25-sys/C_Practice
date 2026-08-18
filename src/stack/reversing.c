603b2b771113b1ba7aa0db43536c27974185187d
dineshpec25-sys <dineshp.ec25@bitsathy.ac.in>
2026-08-18 20:12:45 +0530

    string reversing

--- src/stack/reversing.c
+++ src/stack/reversing.c
@@ -1,32 +1,49 @@
 #include <stdio.h>
+#include<string.h>
 #define MAX 100
 int top = -1;
 char stack[MAX];
 
 void PUSH(int data)
 {
+	if(top == MAX-1)
+	{
+		printf("Stack Overflow\n");
+		return ;
+	}
 	stack[++top]=data;
 }
 
-void POP(char *str)
+int POP()
 {
-	stack[top--]=stack
-void reverse(char *str)
-{
-	for(int i = 0; str[i] != '\0'; i++)
-	{	
-		PUSH(str[i]);
+	if(top == -1)
+	{
+		printf("Stack Underflow\n");
+		return 0;
+	}
+	int temp = stack[top];
+	top--;
+	return temp;
+}
 
 int main()
 {
 	char str[MAX];
+	printf("Enter the string : ");
 	fgets(str, MAX, stdin);
 
 	str[strcspn(str, "\n")] = '\0'; // To remove the '\n' at the end of the string
-	
-	reverse(str);
-	display(str);
+	int count = strlen(str);	
+	for(int i = 0; str[i] != '\0'; i++)
+	{
+		PUSH(str[i]);
+	}
 
+	for(int i = 0; i < count; i++)
+	{
+		str[i] = POP();
+	}
+	printf("The string : %s\n", str);
 	return 0;
 }
 
